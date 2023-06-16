80e9c56493847a572f8c221f572b9ae22fe30818
kushal-lukhi <lukhikushal9@gmail.com>
2023-06-15 17:38:02 -0700

    Done

---
+++ VINIT2(1.C
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<conio.h>
+
+void main(){
+int a,b;
+clrscr();
+a=12;
+b=6;
+printf("Addition of %d and %d is %d\n",a,b,a+b);
+printf("Subtraction of %d and %d is %d\n",a,b,a-b);
+printf("Multiplicatio of %d and %d is %d\n",a,b,a*b);
+printf("Division of %d and %d is %d\n",a,b,a/b);
+printf("Modulo of %d and %d is %d\n",a,b,a%b);
+
+getch();
+
+}
